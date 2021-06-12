0a4cf2e1b70b1ab08334bc9aa614a9958c254865
19diwakar <diwakersingh31@gmail.com>
2021-06-12 15:17:18 +0530

    splash screen ui part is done

--- app/src/main/java/co/diwakar/trivia/SplashActivity.kt
+++ app/src/main/java/co/diwakar/trivia/SplashActivity.kt
@@ -3,9 +3,10 @@ package co.diwakar.trivia
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setTheme(R.style.SplashScreenTheme)
+        setContentView(R.layout.activity_splash)
     }
 }
